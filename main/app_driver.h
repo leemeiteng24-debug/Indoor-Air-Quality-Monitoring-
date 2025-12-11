1a5a90244d3ddcc30bd73a21962a436d449cf444
leemeiteng24-debug <leemeiteng24@gmail.com>
2025-12-11 16:55:40 +0800

    Update app_driver.h

--- main/app_driver.h
+++ main/app_driver.h
@@ -11,4 +11,6 @@ int read_temp(void);       // °C
 int read_humidity(void);   // %
 int read_CO2(void);        // ppm
 
+esp_err_t app_driver_set_gpio(const char *param_name, bool state);
+
 #endif // APP_DRIVER_H
